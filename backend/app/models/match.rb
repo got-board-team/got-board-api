5ca96450d8a91b275be89a9e8638fcb743df49ba
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2015-04-01 22:10:35 -0300

    Preparing match and player associations

--- backend/app/models/match.rb
+++ backend/app/models/match.rb
@@ -2,5 +2,11 @@ class Match < ActiveRecord::Base
 
   has_many :players
   has_one :board
+  # has_many :decks
+  # has_one :wildlings_deck
+  # has_one :westeros_one_deck
+  # has_one :westeros_two_deck
+  # has_one :westeros_three_deck
+  # has_one :tides_of_battle_deck # optional
 
 end
--- backend/app/models/player.rb
+++ backend/app/models/player.rb
@@ -4,6 +4,48 @@ class Player < ActiveRecord::Base
 
   belongs_to :match
 
+  # has_many :house_cards
+  # has_many :available_house_cards,
+  #   -> { where status: 'available' }, class_name: 'HouseCard'
+  # has_many :discarded_house_cards,
+  #   -> { where status: 'discarded' }, class_name: 'HouseCard'
+
+  # has_many :power_tokens
+  # has_many :available_power_tokens,
+  #   -> { where status: 'available' }, class_name: 'PowerToken'
+  # has_many :placed_power_tokens,
+  #   -> { where status: 'placed' }, class_name: 'PowerToken'
+  # has_many :pooled_power_tokens,
+  #   -> { where status: 'pooled' }, class_name: 'PowerToken'
+
+  # has_many :order_tokens
+  # has_many :available_order_tokens,
+  #   -> { where status: 'available' }, class_name: 'OrderToken'
+  # has_many :placed_order_tokens,
+  #   -> { where status: 'placed' }, class_name: 'OrderToken'
+
+  # has_many :dominance_tokens # optionals
+  # has_one :iron_throne_dominance_token,
+  #   -> { where type: 'iron_throne' }, class_name: 'DominanceToken' # optional
+  # has_one :fiefdoms_dominance_token,
+  #   -> { where type: 'fiefdoms' }, class_name: 'DominanceToken' # optional
+  # has_one :kings_court_dominance_token,
+  #   -> { where type: 'kings_court' }, class_name: 'DominanceToken' # optional
+
+  # has_many :units
+  # has_many :available_units,
+  #   -> { where status: 'available' }, class_name: 'Unit'
+  # has_many :placed_units,
+  #   -> { where status: 'placed' }, class_name: 'Unit'
+  # has_many :ship_units,
+  #   -> { where type: 'ship' }, class_name: 'Unit'
+  # has_many :footman_units,
+  #   -> { where type: 'footman' }, class_name: 'Unit'
+  # has_many :knight_units,
+  #   -> { where type: 'knight' }, class_name: 'Unit'
+  # has_many :siege_engine_units,
+  #   -> { where type: 'siege_engine' }, class_name: 'Unit'
+
   validates :match_id, presence: true
   validates :house, presence: true,
     inclusion: {in: HOUSES},
