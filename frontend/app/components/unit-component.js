85fa6540b416ca491f0572c9affd9497b0a5369d
Rafael B. Tauil <rafael@tauil.com.br>
2015-04-16 01:01:28 -0300

    Fixes loading unit in a component from model

--- frontend/app/components/unit-component.js
+++ frontend/app/components/unit-component.js
@@ -4,15 +4,21 @@ export default Ember.Component.extend({
   tagName: "foreignObject",
   classNames: ["piece"],
   attributeBindings: ["data-id", "data-type", "x", "y", "height", "width"],
-  setDefaultProperties: function(){
-    var unit = this.unit;
-    console.log(unit.id);
-    console.log("house", unit.house);
-    //this.setProperties({ "data-id": unit.id,
-                         //"data-type": unit.type,
-                         //"width": "100%",
-                         //"height": "100%",
-    //});
-    this.setProperties(unit.position);
-  }.on("init"),
+  "data-id": function() {
+    return this.unit.id;
+  }.property('unit.id'),
+  "data-type": function() {
+    var unit = this.get('unit');
+    return unit.get('type');
+  }.property('unit.type'),
+  "width": "100%",
+  "height": "100%",
+  "x": function() {
+    var unit = this.get('unit');
+    return unit.get('positionX');
+  }.property('unit.positionX'),
+  "y": function() {
+    var unit = this.get('unit');
+    return unit.get('positionY');
+  }.property('unit.positionY')
 });
