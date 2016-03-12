c0781ada0e226362a928979539be28766325319c
leandrost <leandro.s.thimoteo@gmail.com>
2016-03-11 21:10:19 -0300

    Create matches action

--- spec/controllers/api/v1/matches_controller_spec.rb
+++ spec/controllers/api/v1/matches_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 describe Api::V1::MatchesController, type: :controller do
+  let(:json) { JSON.parse(response.body) }
+
+  describe "#create" do
+    it "creates a match" do
+      expect { post :create }.to change(Match, :count).by(1)
+    end
+
+    it "returns created match" do
+      post :create
+      expect(json[:match]).to eq Match.last.as_json
+    end
+  end
 
 end
