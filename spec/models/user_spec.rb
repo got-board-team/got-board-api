b357a471d78e2194f7fe478f1c7dd8a867b724b4
Rafael B. Tauil <rafael@tauil.com.br>
2016-03-18 21:23:32 -0300

    Adds email validation spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe User, type: :model do
   describe 'relations' do
     it { is_expected.to have_many(:players) }
   end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:email) }
+  end
 end
