9e7f442d95cf27af55f2b30c48bddbc62a99ba9e
Rafael B. Tauil <rafael@tauil.com.br>
2016-03-21 23:41:53 -0300

    Creates, updates and authorizes user in API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+  attr_accessor :current_user
+
+  before_action :authenticate
+
+  private
+
+  def authenticate
+    token = request.headers['authorization'].gsub('Bearer ', '')
+    current_user = User.find_by(token: token)
+    current_user.present?
+  end
 end
