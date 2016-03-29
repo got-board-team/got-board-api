8ddeeb8579afe234d489e2189de911218071e0cd
leandrost <leandro.s.thimoteo@gmail.com>
2016-03-29 01:13:55 -0300

    Create new match with number of players

--- app/controllers/api/v1/matches_controller.rb
+++ app/controllers/api/v1/matches_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::MatchesController < ApplicationController
   end
 
   def create
-    match = MatchSetupService.create!
+    match = MatchSetupService.create!(number_of_players: match_params[:players_count])
     render json: match, include: [:players]
   end
 
@@ -14,5 +14,11 @@ class Api::V1::MatchesController < ApplicationController
   def show
     match = Match.find(params[:id])
     render json: match, include: [:players]
+  end
+
+  private
+
+  def match_params
+    params.require(:match).permit(:players_count)
   end
 end
