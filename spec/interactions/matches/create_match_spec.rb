93b73c85c8c673c2620c2a7056264eed37440a36
leandrost <leandro.s.thimoteo@gmail.com>
2016-04-30 15:16:32 -0300

    setup tracks positions

--- spec/interactions/matches/create_match_spec.rb
+++ spec/interactions/matches/create_match_spec.rb
@@ -61,5 +61,89 @@ describe CreateMatch, type: :interactions do
         expect(house_names).to eq %w(Baratheon Lannister Stark)
       end
     end
+
+    shared_examples :setup_default_positions do
+      let(:match) { ap 0; described_class.run!(number_of_players: 6) }
+
+      subject do
+        match.players.find_by_house(house)
+      end
+
+      describe "positions" do
+        it "sets defaults" do
+          expect(subject.throne_position).to eq throne
+          expect(subject.blade_position).to eq blade
+          expect(subject.raven_position).to eq raven
+          expect(subject.supply_position).to eq supply
+          expect(subject.victory_position).to eq victory
+        end
+      end
+    end
+
+    context "with Baratheon house" do
+      let(:house) { "Baratheon" }
+      let(:throne) { 1 }
+      let(:blade) { 5 }
+      let(:raven) { 4 }
+      let(:supply) { 2 }
+      let(:victory) { 1 }
+
+      it_behaves_like :setup_default_positions
+    end
+
+    context "with Lannister house" do
+      let(:house) { "Lannister" }
+      let(:throne) { 2 }
+      let(:blade) { 6 }
+      let(:raven) { 1 }
+      let(:supply) { 2 }
+      let(:victory) { 1 }
+
+      it_behaves_like :setup_default_positions
+    end
+
+    context "with Stark house" do
+      let(:house) { "Stark" }
+      let(:throne) { 3 }
+      let(:blade) { 4 }
+      let(:raven) { 2 }
+      let(:supply) { 1 }
+      let(:victory) { 2 }
+
+      it_behaves_like :setup_default_positions
+    end
+
+    context "with Greyjoy house" do
+      let(:house) { "Greyjoy" }
+      let(:throne) { 5 }
+      let(:blade) { 1 }
+      let(:raven) { 6 }
+      let(:supply) { 2 }
+      let(:victory) { 1 }
+
+      it_behaves_like :setup_default_positions
+    end
+
+    context "with Tyrell house" do
+      let(:house) { "Tyrell" }
+      let(:throne) { 6 }
+      let(:blade) { 2 }
+      let(:raven) { 5 }
+      let(:supply) { 2 }
+      let(:victory) { 1 }
+
+      it_behaves_like :setup_default_positions
+    end
+
+    context "with Martell house" do
+      let(:house) { "Martell" }
+      let(:throne) { 4 }
+      let(:blade) { 3 }
+      let(:raven) { 3 }
+      let(:supply) { 2 }
+      let(:victory) { 1 }
+
+      it_behaves_like :setup_default_positions
+    end
   end
 end
